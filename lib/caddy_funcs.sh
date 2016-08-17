a7a9f6a5a8f5b1f89ea9e22c728aedec19b98a71
Piotr Usewicz <piotr@layer22.com>
2016-08-17 20:29:45 +0200

    Caddy buildpack initial version

---
+++ lib/caddy_funcs.sh
@@ -0,0 +1,44 @@
+function caddy_tarball() {
+  echo "caddy_linux_amd64_custom.tar.gz"
+}
+
+function download_caddy() {
+  caddy_package_url="https://caddyserver.com/download/build?os=linux&arch=amd64&features=${caddy_features}"
+
+  # If a previous download does not exist, then always re-download
+  if [ ! -f ${cache_path}/$(caddy_tarball) ]; then
+    clean_caddy_downloads
+
+    # Set this so elixir will be force-rebuilt
+    caddy_changed=true
+
+    output_section "Fetching Caddy"
+    curl -s ${caddy_package_url} -o ${cache_path}/$(caddy_tarball) || exit 1
+  else
+    output_section "Using cached Caddy"
+  fi
+}
+
+function clean_caddy_downloads() {
+  rm -rf ${cache_path}/${caddy_tarball}
+}
+
+function install_caddy() {
+  caddy_bin="caddy"
+  bin_path="${build_path}/bin"
+  build_path="${tmp_path}/caddy"
+
+  output_section "Installing Caddy $(caddy_changed)"
+
+  mkdir ${bin_path}
+  tar zxf ${cache_path}/$(caddy_tarball) -C ${build_path} --strip-components=1
+  chmod +x ${build_path}/${caddy_bin}
+  mv ${build_path}/${caddy_bin} ${bin_path}
+  rm -rf ${build_path}
+}
+
+function caddy_changed() {
+  if [ $caddy_changed = true ]; then
+    echo "(changed)"
+  fi
+}
